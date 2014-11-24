dde05affee8fa07e01fc3ae96778bb5180f458d3
kirkira <kirkirvik@gmail.com>
2014-11-24 18:27:15 +0100

    remove syntax_M2

--- syntax_M2/wiktionary_dumper.py
+++
@@ -1,8 +0,0 @@
-
-import xml.etree.ElementTree as ET
-tree = ET.parse('/home/slim/Git/syntax_M2/suffixes_xml.php')
-root = tree.getroot()
-print root.tag
-for suffixe in root.iter("a") :
-	suff = suffixe.get('title')
-	print suff
