0e9ccddab4f26f88f3ea0504a7e8d5e112be6cdd
slim <walajadi@yahoo.fr>
2014-12-05 12:47:57 +0100

    ajout des classes

--- process.py
+++ process.py
@@ -1,10 +1,10 @@
 #-*- coding: utf-8 -*-
-from count_tags import TAGS
-from input_g1 import getUnknownWords
+#from count_tags import TAGS
+#from input_g1 import getUnknownWords
 import codecs
-
+'''
 class_reducer = {
-	'VS':'V'
+	'VS':'V',
 	'VIMP':'V'
 	'VINF':'V'
 	'VPP':'V'
@@ -21,35 +21,78 @@ class_reducer = {
 	'PROWH':'X'
 	'PROREL':'X'
 }
+'''
+class AffixGuesser :
 
-class SuffixGuesser :
 	#class reconnaisant les suffixes
 
-	def __init__(self, suffixes_file) :
+	def __init__(self, suffixes_file, prefixes_file) :
 		self.suffixes = []
+		self.prefixes = []
 		with codecs.open(suffixes_file, 'r', 'utf-8') as inputfile :
 			for line in inputfile :
 				self.suffixes.append(line.rstrip('\n\r'))
 
 		inputfile.close()
 
-		def listeSuffPossibles(self, mot) :
-			#on aura une liste possible de suffixes pour un mot donné
-			results = set([])
-			for suff in self.suffixes :
-				if word.endswith(suff) :
-					results.append(suff)
+		with codecs.open(prefixes_file, 'r', 'utf-8') as inputfile_ :
+			for line in inputfile_ :
+				self.prefixes.append(line.rstrip('\n\r'))
+		
+		inputfile_.close()
+
+	def listeSuffPossibles(self, unknownWord) :
+		#on aura une liste possible de suffixes pour un mot donné
+		results = set([])
+		for suff in self.suffixes :
+			if unknownWord.endswith(suff) :
+				results.add(suff)
+		return results
+
+	def listePrefPossibles(self, unknownWord) :
+		results = set([])
+		for pref in self.prefixes:
+			if unknownWord.startswith(pref) or unknownWord.startswith(pref+'-') :
+				results.add(pref)
+		return results
 
 
-			return results
 
-def Analyser :
+class Analyser :
 	
 	#analyse morphologique
 
-	def __init__(self, suffReco, probaSuff) :
-		self.suff_guesser = SuffixGuesser()
+	def __init__(self, affixGuesser, probaSuff, lexique) :
+		
+		self.aff_guesser = affixGuesser
 		self.proba_suffix = probaSuff
+		self.lexique = lexique
 
+	def scoreToken(self, token) :
+		pass
 
+	def checkStem(self, token) :
 
+		suffixes = self.affixGuesser.listeSuffPossibles(token)
+		prefixes = self.affixGuesser.listePrefPossibles(token)
+
+		for suff in suffixes :
+			if token[:len(suff)] in self.lexique :
+				#bingo trouvé
+				return True #or something like this
+			else :
+				for pref in prefixes :
+					if token[len(pref):] in self.lexique :
+						return (token, pref)
+					elif token[len(pref):len(suff)] in self.lexique :
+						return (token, (pref, suff))
+		return (token, null)
+
+ 
+if __name__ == '__main__':
+	suff = r'pertinent_suffixes.txt'
+	pref = r'pertinent_prefixes.txt'
+
+	guesser = AffixGuesser(suff, pref)
+	print guesser.listePrefPossibles(u'antinucléaire')
+	print guesser.listeSuffPossibles(u'industrialisation')
