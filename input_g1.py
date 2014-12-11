2d33efc9687303b28d751836d16112f5fc6c7af6
slim <walajadi@yahoo.fr>
2014-12-12 00:11:36 +0100

    prefix processed and new groupe 1 reading input def

--- input_g1.py
+++ input_g1.py
@@ -1,6 +1,8 @@
 #-*- coding: utf-8 -*-
 
 import codecs
+#from process import Analyser, AffixGuesser
+
 
 def getUnknownWords(infile, lexiqueList) :
 	unknowns = []
@@ -18,9 +20,29 @@ def getLexique(infile) :
 	liste = []
 	with codecs.open(infile, 'r', 'utf-8') as inputFileObj:
 		for line in inputFileObj :
-			data = line.split('\n')
+			data = line.split('\t')
 			liste.append(data[0])
 	inputFileObj.close()
 	return liste
 
 
+def readFileG1(infile, suff, pref, lex, dict_proba) :
+	guesser = AffixGuesser(suff, pref)
+	analyseur = Analyser(guesser, dict_proba, lex)
+	lexique = getLexique(lex)
+	outputfile = 'output-g2.txt'
+	output = codcs.open('w', 'utf-8')
+	with codecs.open(infile, 'w', 'utf-8') as inputFileObj:
+		for line in inputFileObj :
+			texte = line.split(' ')
+			for word in texte :
+				if word.startswith('{') : 
+					output.write(word+" ")
+				else :
+					if word in lexique :
+						output.write(word+" ")
+					else :
+						tag = analyseur.getTag(word)
+						output.write(word+"/"+tag[0]+"/"+tag[1])
+	inputFileObj.Close()
+	
--- process.py
+++ process.py
@@ -1,12 +1,12 @@
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals
-#from count_tags import TAGS
-#from input_g1 import getUnknownWords
+
 import re
 import codecs
-
 from input_g1 import getLexique
 from probas2dict import dict_probas
+from pprint import pprint
+
 class_reducer = {
 	'VS':'V',
 	'VIMP':'V',
@@ -56,7 +56,6 @@ class AffixGuesser :
 		return results
 
 
-
 class Analyser :
 	
 	#analyse morphologique
@@ -64,7 +63,7 @@ class Analyser :
 	def __init__(self, affixGuesser, probaSuff, lexique) :
 		
 		self.aff_guesser = affixGuesser
-		self.proba_suffix = probaSuff
+		self.probaTags = probaSuff
 		self.lexique = lexique
 		self.last_suffix = ''
 
@@ -85,10 +84,6 @@ class Analyser :
 		if stem[-1] == stem[-2] :
 			pattern = '^'+stem+'?.{,2}$'
 		
-		#if re.search(pattern, self.lexique) != None : #le 2eme arg de search doit etre une chaine
-		#	return True
-		#else :
-		#	return False
 		for word in self.lexique:
 			if re.search(pattern, word) != None :
 				return True
@@ -103,23 +98,31 @@ class Analyser :
 		suffixes = sorted(suffixes,key=len, reverse = True)
 		for suff in  suffixes:
 			stem = token[:len(token) - len(suff)]
-			print stem[:-2]+ " stem coupé"
 			if self.searchLexique(stem) :
 				#bingo trouvé
 				suffix = suff
 				return (token, stem, suff) #or something like this
 			else :
-				print "prefixes ici"
 				for pref in prefixes :
+					stem = token[len(pref):len(token) - len(suff)]
 					if self.searchLexique(stem) :
-						return (token, stem, null)
-					elif self.searchLexique(stem) :
-						print stem
-						return (token, stem, suff)
+						return (token, stem,suff)#ici on peux retourner le prefixe ??
+					#elif self.searchLexique(stem) :
+					#	return (token, stem, suff)
+		try :
+			return (token, stem, suffixes[0])
+		except KeyError:
+			return (token, stem, None)
 
-		return (token, stem, suffixes[0])
-
-
+	def getTag(self, token) :
+		probasTags = []
+		data = self.checkStem(token)
+		affixes = data[2]
+		print affixes
+		try :
+			return self.probaTags[affixes]
+		except KeyError:
+			return None
 if __name__ == '__main__':
 
 	suff = r'pertinent_suffixes.txt'
@@ -128,8 +131,10 @@ if __name__ == '__main__':
 	probas_file = r'cat_probas.txt'
 	lexique =  getLexique(lex_path)
 	dict_proba = dict_probas(probas_file)
+	#pprint(dict_proba)
 	guesser = AffixGuesser(suff, pref)
 	#print guesser.listePrefPossibles(u'antinucléaire')
 	#print guesser.listeSuffPossibles(u'industrialisation')
 	analyseur = Analyser(guesser, dict_proba , lexique)
-	print analyseur.checkStem('justicier')
+	#print analyseur.checkStem('anticonsititutionnellement')
+	print analyseur.getTag("antisocialisme")
