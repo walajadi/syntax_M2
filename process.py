14a97be4ccdfa4820bdd7b2d331f81b3a0029eda
slim <walajadi@yahoo.fr>
2014-12-11 13:03:00 +0100

    debug

--- process.py
+++ process.py
@@ -1,8 +1,10 @@
 #-*- coding: utf-8 -*-
+from __future__ import unicode_literals
 #from count_tags import TAGS
 #from input_g1 import getUnknownWords
 import re
 import codecs
+
 from input_g1 import getLexique
 from probas2dict import dict_probas
 class_reducer = {
@@ -70,45 +72,54 @@ class Analyser :
 		return self.proba_suffix[suff]
 	
 	def searchLexique(self, stem) :
-		pattern = '^'+stem+'.{,2}$'
+
+		pattern = '^'+stem+'\w{,2}$'
 		if re.search('(c|ç)$', stem) != None :
-			pattern = '^'+stem[:-1]+'(c|ç|que|ch).{,2}$'
+			pattern = '^'+stem[:-1]+'(c|ç|que|ch)\w{,2}$'
 		if re.search('ch$', stem) != None :
-			pattern = '^'+stem[:-2]+'(c|ç|que|ch).{,2}$'
+			pattern = '^'+stem[:-2]+'(c|ç|que|ch)\w{,2}$'
 		if re.search('que$', stem) != None :
-			pattern = '^'+stem[:-3]+'(c|ç|que|ch).{,2}$'
+			pattern = '^'+stem[:-3]+'(c|ç|que|ch)\w{,2}$'
 		if re.search('(al|el)$', stem) != None :
-			pattern = '^'+stem[:-2]+'(al|el).{,2}$'
+			pattern = '^'+stem[:-2]+'(al|el)(\w)*$'
 		if stem[-1] == stem[-2] :
 			pattern = '^'+stem+'?.{,2}$'
 		
-		if re.search(pattern, self.lexique) != None :
-			return True
-		else :
-			return False
+		#if re.search(pattern, self.lexique) != None : #le 2eme arg de search doit etre une chaine
+		#	return True
+		#else :
+		#	return False
+		for word in self.lexique:
+			if re.search(pattern, word) != None :
+				return True
+			
+		return False
 
 	def checkStem(self, token) :
 
-		suffixes = self.affix_guesser.listeSuffPossibles(token)
-		prefixes = self.affix_guesser.listePrefPossibles(token)
+		suffixes = self.aff_guesser.listeSuffPossibles(token)
+		prefixes = self.aff_guesser.listePrefPossibles(token)
 		stem = token
-
-		for suff in sort_truc(suffixes) :
-			if searchLexique(token[:len(suff)]) :
+		suffixes = sorted(suffixes,key=len, reverse = True)
+		for suff in  suffixes:
+			stem = token[:len(token) - len(suff)]
+			print stem[:-2]+ " stem coupé"
+			if self.searchLexique(stem) :
 				#bingo trouvé
 				suffix = suff
-				stem = token[:len(suff)]
 				return (token, stem, suff) #or something like this
 			else :
+				print "prefixes ici"
 				for pref in prefixes :
-					if searchLexique(token[len(pref):]) :
+					if self.searchLexique(stem) :
 						return (token, stem, null)
-					elif searchLexique(token[len(pref):len(suff)]) :
+					elif self.searchLexique(stem) :
+						print stem
 						return (token, stem, suff)
 
 		return (token, stem, suffixes[0])
 
- 
+
 if __name__ == '__main__':
 
 	suff = r'pertinent_suffixes.txt'
@@ -121,3 +132,4 @@ if __name__ == '__main__':
 	#print guesser.listePrefPossibles(u'antinucléaire')
 	#print guesser.listeSuffPossibles(u'industrialisation')
 	analyseur = Analyser(guesser, dict_proba , lexique)
+	print analyseur.checkStem('justicier')
